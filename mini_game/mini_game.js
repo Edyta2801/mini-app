ab612354e08073e9d14bfa7ee927df605413a1ba
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-16 17:08:03 +0100

    player position

--- mini_game/mini_game.js
+++ mini_game/mini_game.js
@@ -3,9 +3,9 @@ function Game(selector, boardDimension) {
         document.querySelector(selector)
         :
         document.querySelector('body')
-    this.boardDimension=3
-    this.cellDimension=(100/this.boardDimension)+'%'
-    this.playerPosition={
+    this.boardDimension = 3
+    this.cellDimension = (100 / this.boardDimension) + '%'
+    this.playerPosition = {
         x: 1,
         y: 0
     }
@@ -14,25 +14,64 @@ function Game(selector, boardDimension) {
         [0, 0, 0],
         [0, 0, 0]
     ]
-    this.gameBoardArray[this.playerPosition.y][this.PlayerPosition.x]=1
-    this.gameBoard=null
+
+
+    this.gameBoard = null
     this.init()
 }
-Game.prototype.init=function(){
+Game.prototype.init = function () {
     this.makeGameBoard()
     this.render()
+    this.startListeningArrowKeys()
 }
 
-Game.prototype.makeGameBoard=function(cell){
-const boardElement=document.createElement('div')
-boardElement.style.width='500px'
-boardElement.style.height='500px'
-boardElement.style.display='flex'
-boardElement.style.flex='wrap'
+Game.prototype.makeGameBoard = function (cell) {
+    const boardElement = document.createElement('div')
+    boardElement.style.width = '500px'
+    boardElement.style.height = '500px'
+    boardElement.style.display = 'flex'
+    boardElement.style.flexWrap = 'wrap'
 
-this.gameBoard=boardElement
-this.container.appendChild(boardElement)
+    this.gameBoard = boardElement
+    this.container.appendChild(boardElement)
 }
+Game.prototype.move = function (y, x) {
+    const newPlayerPosition = {
+        x: this.playerPosition.x + x,
+        y: this.playerPosition.y + y,
+
+    }
+    this.gameBoardArray[this.playerPosition.y][this.playerPosition.x] = 0
+    this.gameBoardArray[this.newPlayerPosition.y][this.newPlayerPosition.x + x] = 1
+    this.playerPosition = newPlayerPosition
+}
+
+
+
+Game.prototype.startListeningArrowKeys = function () {
+    window.addEventListener(
+        'keydown',
+        event => {
+            event.preventDefault()
+            switch (event.keys) {
+                case 'ArrowUp':
+                    this.move(-1, 0)
+                    break
+                case 'ArrowDown':
+                    this.move(1, 0)
+                    break
+                case 'ArrowLeft':
+                    this.move(0, -1)
+                    break
+                case 'ArrowRight':
+                    this.move(0, 1)
+                    break
+            }
+        }
+    )
+}
+
+
 Game.prototype.render = function () {
     this.gameBoardArray.forEach(row => {
         row.forEach(cell => {
@@ -48,7 +87,7 @@ Game.prototype.renderSingleCell = function (cell) {
     if (cell == 0) {
         cellElement.style.backgroundColor = 'grey'
     } else {
-        cellElement.style.backgroundColor= 'black'
-        }
-    this.gameBoard.appendChild(cellElement)
+        cellElement.style.backgroundColor = 'black'
     }
+    this.gameBoard.appendChild(cellElement)
+}
